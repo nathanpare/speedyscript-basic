78c7031964f5e840c7489942bd4cd5154b242f3d
Nathan Pare <nathanpare@Nathans-MBP.lan>
2023-02-15 19:52:44 -0800

    complete header and routing

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1 className='pt-24 flex justify-center text-xl text-[rgb(251,247,245)]'>BlackBoard Development</h1>
+    <div className='h-24 flex justify-between
+     items-center px-5'>
+      <p onClick={() => router.push("/")}
+        className='sm:text-lg md:text-xl lg:text-2xl cursor-pointer
+       text-[rgb(251,247,245)]'>BlackBoard</p>
+      <div className='flex justify-between'>
+        <p onClick={() => router.push("/services")}
+          className='sm:text-sm md:text-md lg:text-lg hover:underline
+         cursor-pointer text-[rgb(251,247,245)]'>Our Services</p>
+        <p onClick={() => router.push("/portfolio")}
+          className='sm:text-sm md:text-md lg:text-lg cursor-pointer
+         hover:underline pl-4 text-[rgb(251,247,245)]'>Our Work</p>
+        <p onClick={() => router.push("/mission")}
+          className='sm:text-sm md:text-md lg:text-lg cursor-pointer
+         hover:underline pl-4 text-[rgb(251,247,245)]'>Our Mission</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header;
---
+++ pages/mission.js
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import React from 'react';
+
+const Mission = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Mission</h1>
+    </div>
+  )
+}
+
+export default Mission;
