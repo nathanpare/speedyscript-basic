06572700a94a0cd62257e23a123797ccc44d985b
Nathan Pare <nathanpare@Nathans-MBP.lan>
2023-03-13 20:41:32 -0700

    home css

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
       <Header />
 
       <main>
-        <div className='text-[rgb(251,247,245)] flex flex-col items-center pt-24'>
-          <div className='text-2xl sm:text-4xl font-semibold md:text-5xl lg:text-6xl'>
+        <div className='text-[rgb(251,247,245)] flex flex-col items-center pt-32'>
+          <div className='text-3xl sm:text-4xl font-semibold md:text-5xl lg:text-6xl'>
             <Typewriter
               words={['Blacksand Web Design', 'Your new Web Artists!', 'Your new SEO Specialists!', 'Your new Marketing Team!']}
               loop={4}
