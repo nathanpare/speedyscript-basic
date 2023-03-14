03fe763fa70056b20b7144cbb16124c9d776c961
Nathan Pare <nathanpare@Nathans-MBP.lan>
2023-03-13 21:02:29 -0700

    pt

--- pages/services.js
+++ pages/services.js
@@ -74,7 +74,7 @@ const Services = () => {
       </section>
 
       <section id='service-2' className='snap-start h-screen flex flex-col
-       items-center pt-10'>
+       items-center pt-20'>
         <div id='top' className='text-2xl sm:text-3xl md:text-4xl flex flex-row items-center mx-auto py-24'>
           <motion.div
             className='px-5 mx-auto'
@@ -127,7 +127,7 @@ const Services = () => {
       </section>
 
       <section id='service-3' className='snap-center h-screen flex flex-col 
-       items-center pt-8'>
+       items-center pt-16'>
         <div id='top-2' className='text-2xl sm:text-3xl md:text-4xl flex flex-row items-center py-24'>
           <motion.div
             className='p-2 mx-auto mr-24'
@@ -180,7 +180,7 @@ const Services = () => {
       </section>
 
       <section id='service-4' className='snap-center h-screen flex flex-col 
-       items-center pt-5'>
+       items-center pt-10'>
         <div id='top-3' className='text-2xl sm:text-3xl md:text-4xl flex flex-row items-center py-24'>
           <motion.div
             className='px-5 mx-auto mr-24'
